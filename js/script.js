f339945a16a86dd1810ba3d2775d68987aee8b51
angelarenata <angelarenata.a@hotmail.com>
2023-06-20 22:43:44 -0300

    dando um destino quando houver o submit, salvando no localStorage

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
 import eCPF from "./validar-cpf.js";
 import eMaiorDeIdade from "./validar-idade.js";
 const inputs = document.querySelectorAll("[required]");
+const formulario = document.querySelector("[data-formulario]");
 
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const listaRespostas = {
+        "nome": e.target.elements["nome"].value,
+        "email": e.target.elements["email"].value,
+        "cpf": e.target.elements["cpf"].value,
+        "nascimento": e.target.elements["nascimento"].value,
+        "senha": e.target.elements["senha"].value,
+        "confirma": e.target.elements["confirma"].value,
+    }
+    localStorage.setItem("cadastro", JSON.stringify(listaRespostas));
+
+    window.location.href = './sucesso.html';
+})
 
 inputs.forEach((campo) => {
     campo.addEventListener("blur", () => verificarCampo(campo));
@@ -38,7 +54,14 @@ const mensagens = {
         valueMissing: 'O campo de data de nascimento não pode estar vazio.',
         customError: 'Você deve ser maior que 18 anos para se cadastrar.'
     },
-    termos: {
+    senha: {
+        valueMissing: 'A senha não pode estar vazia.',
+        tooShort: "A senha não tem caractéres suficientes.",
+    },
+    confirma: {
+        valueMissing: 'A confirmação de senha não pode estar vazia.',
+    },
+    checkbox: {
         valueMissing: 'Você deve aceitar nossos termos antes de continuar.',
     }
 }
@@ -56,8 +79,7 @@ function verificarCampo(campo) {
 
     tiposDeErro.forEach(erro => {
         if (campo.validity[erro]) {
-            mensagem = mensagens[campo.name][erro];
-            console.log(mensagem);
+            mensagem = mensagens[campo.name][erro]
         }
     })
     const mensagemErro = campo.parentNode.querySelector('.mensagem-erro');
